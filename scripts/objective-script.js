9812343b5470cd980a649daaecc81e03d429cefc
majid hatami <majidhatamimalekii@gmail.com>
2022-07-26 11:14:52 +0430

    [JS] adding another script.js file work working on with objective mothods

--- scripts/objective-script.js
+++ scripts/objective-script.js
@@ -4,50 +4,123 @@ window.addEventListener('click',function (e) {
 })
 // 
 // 
+let newTodoContainer = document.querySelector('.todos-list-ul')
 let createNewTodoPanel = {
     form : document.querySelector('.create-todo-form'),
+    allBtns : document.querySelectorAll('.form-btns'),
     titleInput : document.querySelector('.title-input'),
     descriptionInput : document.querySelector('.description-input'),
     linkInput : document.querySelector('.link-input'),
     linkLabelInput : document.querySelector('.link-label'),
     priorityMenu : {
+        prioritiesMenu : document.querySelector('.priorities-options-menu'),
+        openPriorityMenuBtn : document.querySelector('.open-priorities-menu-btn'),
         priorityMenuTitle : document.querySelector('.priorities-menu-title-item'),
         lowPriority : document.querySelector('#low-priority'),
         mediumPriority : document.querySelector('#medium-priority'),
         highPriority : document.querySelector('#high-priority'),
+        priorityLabels : document.querySelectorAll('.priority-labels')
     },
     timers : {
         hours : {
             display : document.querySelector('.todo-timer-hours-display'),
             increaseBtn : document.querySelector('.hours-increase-btn'),
-            decreaseBtn : document.querySelector('hours-decrease-btn'),
+            decreaseBtn : document.querySelector('.hours-decrease-btn'),
+            timeLimit : 12,
             counter : 0
         },
         minutes : {
             display : document.querySelector('.todo-timer-mins-display'),
             increaseBtn:document.querySelector('.minutes-increase-btn'),
             decreaseBtn : document.querySelector('.minutes-decrease-btn'),
+            timeLimit : 60,
             counter : 0
         },
         seconds : {
             display : document.querySelector('.todo-timer-secs-display'),
             increaseBtn:document.querySelector('.seconds-increase-btn'),
             decreaseBtn : document.querySelector('.seconds-decrease-btn'),
+            timeLimit : 60,
             counter : 0
-
         }
     },
     submit : document.querySelector('.create-form-submit-btn')
 }
 
+let newTodoObject = {
+    Id : null,
+    title : null,
+    description : null,
+    link : null,
+    labelOfLink : null,
+    priorityColor : null,
+}
 
-function createNewTodo() {
-    newTodoForm.insertAdjacentHTML('beforeend',`
-    <li class="todos-list-lis" style="box-shadow: -2px 2px 0px 0px ${priorityColor}">
+createNewTodoPanel.allBtns.forEach((e) => {
+    e.addEventListener('click',(e)=>{e.preventDefault()})
+});
+
+
+
+
+createNewTodoPanel.priorityMenu.openPriorityMenuBtn.addEventListener('click',()=>{
+    createNewTodoPanel.priorityMenu.prioritiesMenu.classList.toggle('display-none')
+    
+})
+
+function newTodoTimerHandler(timeLimit,checkIncrease,display,counter) {
+    let isIncrease = checkIncrease
+    if(isIncrease && counter > 0 && counter < timeLimit){
+        counter++
+        display.innerHTML = counter
+    }
+}
+
+createNewTodoPanel.timers.hours.increaseBtn.addEventListener('click',()=>{
+    newTodoTimerHandler(createNewTodoPanel.timers.hours.timeLimit,true,createNewTodoPanel.timers.hours.display,createNewTodoPanel.timers.hours.counter)
+})
+
+createNewTodoPanel.timers.hours.decreaseBtn.addEventListener('click',()=>{
+    newTodoTimerHandler(createNewTodoPanel.timers.hours.timeLimit,false,createNewTodoPanel.timers.hours.display)
+
+})
+
+createNewTodoPanel.timers.minutes.increaseBtn.addEventListener('click',()=>{
+    newTodoTimerHandler(createNewTodoPanel.timers.minutes.timeLimit,true,createNewTodoPanel.timers.minutes.display)
+})
+
+
+
+ function randomNum() {
+    let randomNumber = Math.floor(Math.random()* 900 + 100)
+    return randomNumber
+}
+
+createNewTodoPanel.submit.addEventListener('click',(e)=>{
+    e.preventDefault()
+    newTodoObject.Id = randomNum()
+    newTodoObject.title = createNewTodoPanel.titleInput.value
+    newTodoObject.description = createNewTodoPanel.descriptionInput.value
+    newTodoObject.link = createNewTodoPanel.linkInput.value
+    newTodoObject.labelOfLink = createNewTodoPanel.linkLabelInput.value
+    let low = createNewTodoPanel.priorityMenu.lowPriority.checked
+    let medium = createNewTodoPanel.priorityMenu.mediumPriority.checked
+    let high = createNewTodoPanel.priorityMenu.highPriority.checked
+    createNewTodo(newTodoObject.title,newTodoObject.description,newTodoObject.link,newTodoObject.labelOfLink,low,high,medium)
+})
+
+function createNewTodo(title,description,link,labelOfLink,low,high,medium) {
+
+    if(low) newTodoObject.priorityColor = createNewTodoPanel.priorityMenu.lowPriority.value
+    if(medium)  newTodoObject.priorityColor = createNewTodoPanel.priorityMenu.mediumPriority.value
+    if(high) newTodoObject.priorityColor = createNewTodoPanel.priorityMenu.highPriority.value
+
+    newTodoContainer.insertAdjacentHTML('beforeend',`
+    <li class="todos-list-lis" style="box-shadow: -2px 2px 0px 0px ${newTodoObject.priorityColor}">
     <div class="todos-lis-text">
-        <h3 class="todos-text-title">${newTodoTitle}</h3>
-        <p class="todos-text-description">${newTodoDescription}</p>
-        <a href="${newTodoLinkUrl}" class="todos-text-link">${newTodoLinkLabel}</a>
+        <h3 class="todos-text-title">   ${title}   </h3>
+        <p class="todos-text-description">  ${description} </p>
+        <a href="   ${link}   " class="todos-text-link">  ${labelOfLink}    </a>
         <button class="todos-description-show-btn" title="show more">
             <i class="fas fa-caret-down"></i>
         </button>
@@ -75,4 +148,6 @@ function createNewTodo() {
     </div>
 </li>
     `) 
+
+
 }
--- scripts/script.js
+++ scripts/script.js
@@ -74,52 +74,6 @@ function resetPanel() {
     getNewElement('.priority-labels-title').innerHTML = 'choose priority'
 }
 
-<<<<<<< HEAD
-
-
-let hoursTimersCounter = 0
-let minutesTimersCounter = 0
-let secondsTimersCounter = 0
-let hoursDisplay = getNewElement('.todo-timer-hours-display')
-let minsDisplay = getNewElement('.todo-timer-mins-display')
-let secsDisplay = getNewElement('.todo-timer-secs-display')
-let newTodoTimerBtns = getNewElements('.todo-timer-btns')
-
-newTodoTimerBtns.forEach((e) => {
-    let isIncreaseBtn
-    e.addEventListener('click',(e)=>{
-        if(e.target.parentElement.dataset.typeOfBtn === '1') isIncreaseBtn = true
-        if(e.target.parentElement.dataset.typeOfBtn === '-1') isIncreaseBtn = false
-        
-        let typeOfTime = e.target.parentElement.dataset.typeOfTime
-        NewTodoIncreaseBtns(typeOfTime,isIncreaseBtn)
-        NewTodoDecreaseBtns(typeOfTime,isIncreaseBtn)
-    })
-});
-
-function NewTodoIncreaseBtns(typeOfTime,isIncreaseBtn) {
-    if(typeOfTime === 'hour' && isIncreaseBtn) hoursDisplay.innerHTML = hoursTimersCounter++
-    if(typeOfTime === 'minute' && isIncreaseBtn) minsDisplay.innerHTML = minutesTimersCounter++
-    if(typeOfTime === 'second' && isIncreaseBtn) secsDisplay.innerHTML = secondsTimersCounter++
-}
-function NewTodoDecreaseBtns(typeOfTime,isIncreaseBtn) {
-    if(typeOfTime === 'hour' && !isIncreaseBtn){
-        hoursDisplay.innerHTML = hoursTimersCounter--
-        if(hoursTimersCounter < 0) hoursTimersCounter = 0
-    } 
-    if(typeOfTime === 'minute' && !isIncreaseBtn){
-        minsDisplay.innerHTML = minutesTimersCounter--
-        if(minutesTimersCounter < 0) minutesTimersCounter = 0
-    }
-    
-    if(typeOfTime === 'second' && !isIncreaseBtn) {
-        secsDisplay.innerHTML = secondsTimersCounter--
-        if(secondsTimersCounter < 0) secondsTimersCounter = 0
-        
-    }
-}
-
-=======
 newTodoFormSubmitBtn.addEventListener('click',function (e) {
 
     let newTodoTitle = newTodoFormTitleInput.value
@@ -134,7 +88,6 @@ newTodoFormSubmitBtn.addEventListener('click',function (e) {
     resetPanel()
     closePriorityMenu()
 })
->>>>>>> master
 function createNewTodo(newTodoTitle , newTodoDescription,newTodoLinkUrl,newTodoLinkLabel,lowPriority,mediumPriority,highPriority){
     let priorityColor
     if(lowPriority) priorityColor = lowPriorityInput.value
@@ -175,9 +128,6 @@ function createNewTodo(newTodoTitle , newTodoDescription,newTodoLinkUrl,newTodoL
 </li>
     `)    
 }
-<<<<<<< HEAD
-getNewElement('.open-priorities-menu-btn').addEventListener('click',priorityPanel)
-=======
 
 
 
@@ -186,7 +136,6 @@ getNewElement('.open-priorities-menu-btn').addEventListener('click',priorityPane
 
 newElement('.create-todo-form').addEventListener('click',(e)=>{e.preventDefault()})
 newElement('.open-priorities-menu-btn').addEventListener('click',priorityPanel)
->>>>>>> master
 function priorityPanel(e) {
     openPriorityMenuHandler()
     changePriorityTitle()
